3fa49264948d33fb4a9c997e391397b3f153d6cc
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2019-09-26 20:06:37 +0200

    Moved script.

--- _scripts/genDraft.sh
+++ _scripts/genDraft.sh
@@ -2,24 +2,27 @@
 
 # Generates a draft post for jekyll honoring the jekyll naming format.
 
-exit_error() {
+exit_error(){
     echo "$1"
     exit 1
 }
 
 print_help(){
     echo "Usage:"
-    echo -e "\t`basename "$0"` \"{title}\" \"{category1}\" \"{category2}\" ... \"{categoryN}\""
+    echo -e "\t`basename "$0"` \"{title}\" \"{category1}\" \"{optionalCategory1}\" ... \"{optionalCategoryN}\""
     echo ""
 }
 
-join_by (){
+join_by(){
     local IFS="$1"
     shift
     echo "$*"
 }
 
-[ $# -lt 2 ] && [ "$1" == "help" ] && print_help && exit
+if [ $# -lt 2 ] || ( [ $# ] && [ "$1" == "help" ] )
+then
+    print_help && exit
+fi
 
 [ $# -lt 2 ] && exit_error "not enough arguments"
 
@@ -27,15 +30,16 @@ dd=`date +%Y-%m-%d`
 dt=`date +'%H:%M:%S %z'`
 layout=post
 
+filename=$(echo $1 | sed -e 's/./\L&/g' -e 's/\s/-/g' -e 's/[^[:alnum:]-]//g')
+
 categories=`join_by "," ${@:2}`
 
 FRONT_MATTER="---
 layout: $layout
-title: $1
+title: \"$1\"
 date: $dd $dt
 categories: [$categories]
 ---
 "
 
-echo "$FRONT_MATTER" > "$dd-$1.md"
-echo "$FRONT_MATTER"
+echo "$FRONT_MATTER" > "$dd-$filename.md"
